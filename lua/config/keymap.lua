fa0b79fa48fb414bfc4c95010248da907acb85ef
dobayer <dr.dominik.bayer@gmail.com>
2023-04-11 20:23:51 +0200

    Telescope and Lualine

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,4 +1,9 @@
 -- Key Mappings 
+vim.keymap.set("n", "<leader>e", ":Neotree<CR>")
 
-vim.keymap.set("n", "<leader>t", ":Neotree<CR>")
-
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "Telescope - Find Files"})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "Telescope - Grep"})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Telescope - Find Buffers"})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Telescope - Search Help Tags"})
