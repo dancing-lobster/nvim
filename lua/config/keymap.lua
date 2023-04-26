052bdff2b89f55a55d972468b8f7d88b95aa9c58
dobayer <dr.dominik.bayer@gmail.com>
2023-04-26 11:42:56 +0200

    undotree and whichkey

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,9 +1,15 @@
--- Key Mappings 
+-- Key Mappings
 vim.keymap.set("n", "<leader>e", ":Neotree<CR>")
 
 -- Telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "Telescope - Find Files"})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "Telescope - Grep"})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Telescope - Find Buffers"})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Telescope - Search Help Tags"})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Telescope - Find Files" })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Telescope - Grep" })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Telescope - Find Buffers" })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Telescope - Search Help Tags" })
+
+-- Undotree
+vim.keymap.set('n', '<leader>u', ':UndotreeToggle<CR>', { desc = "Undotree" })
+
+-- Which-Key
+vim.keymap.set('n', '<leader>w', ':WhichKey<CR>', { desc = "WhichKey - Normal" })
--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -29,19 +29,6 @@ return {
         }
     },
     -- Treesitter -------------------------------------------------------------
-    -- WhichKey ---------------------------------------------------------------
-    {
-        "folke/which-key.nvim",
-        config = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-            require("which-key").setup({
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            })
-        end,
-    },
     -- Colors -----------------------------------------------------------------
     {
         -- the colorscheme should be available when starting Neovim
